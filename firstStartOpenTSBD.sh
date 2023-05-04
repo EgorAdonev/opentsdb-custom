cd320c88a4a59f3ba3449999758181e37db05eac
System Administrator <root@MacBook-Air-Marina.local>
2023-05-04 22:53:58 +0300

    Linux version

--- firstStartOpenTSBD.sh
+++
@@ -1,9 +0,0 @@
-cd hbase-2.4.14
-./bin/start-hbase.sh
-#git clone git://github.com/OpenTSDB/opentsdb.git
-cd ..
-cd opentsdb-2.4.1
-#./build.sh
-env COMPRESSION=NONE HBASE_HOME=/opt/hbase-2.4.14 ./src/create_table.sh
-./build/tsdb tsd
-#service opentsdb start
